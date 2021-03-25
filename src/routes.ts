b7aa32e1d87040b1eaa4e1e3424696932f04b5b3
eloilsondosanjos <eloilson.ageedu@gmail.com>
2021-03-25 15:38:37 -0300

    Created NpsController

--- src/routes.ts
+++ src/routes.ts
@@ -2,12 +2,16 @@ import { Router } from 'express';
 import SurveysController from './controllers/SurveysController';
 import UsersController from './controllers/UsersController';
 import SendMailController from './controllers/SendMailController';
+import AnswerController from './controllers/AnswerController';
+import NpsController from './controllers/NpsController';
 
 const router = Router();
 
 const usersController = new UsersController();
 const surveysController = new SurveysController();
 const sendMailController = new SendMailController();
+const answerController = new AnswerController();
+const npsController = new NpsController();
 
 router.get('/users', usersController.show);
 router.post('/users', usersController.create);
@@ -16,6 +20,10 @@ router.get('/surveys', surveysController.show);
 router.post('/surveys', surveysController.create);
 
 router.post('/sendMail', sendMailController.execute);
+
+router.get('/answers/:value', answerController.execute);
+
+router.get('/nps/:survey_id', npsController.execute);
 
 
 export default router;
