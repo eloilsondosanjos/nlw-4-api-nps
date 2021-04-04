f27c2e1325c359091162f817881522531ea88d0a
eloilsondosanjos <eloilson.ageedu@gmail.com>
2021-04-04 15:37:53 -0300

    Customized error handling implemented

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,10 +1,24 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import UsersRepository from '../repositories/UsersRepository';
+import * as yup from 'yup';
+import { AppError } from '../errors/AppError';
+
 
 class UsersController {
   async create(request: Request, response: Response) {
     const { name, email } = request.body; 
+
+    const schema = yup.object().shape({
+      name: yup.string().required("Nome é obrigatório"),
+      email: yup.string().email().required("E-mail é obrigatório"),     
+    });
+
+    try {
+      await schema.validate(request.body, { abortEarly: false });      
+    } catch (error) {
+      throw new AppError(error);
+    }
 
     const usersRepository = getCustomRepository(UsersRepository);
 
@@ -13,7 +27,7 @@ class UsersController {
     });
 
     if(userAlreadyExists) {
-      return response.status(400).json({error: "User aleready exists!"});
+      throw new AppError("User aleready exists")
     }
 
     const user = usersRepository.create({
